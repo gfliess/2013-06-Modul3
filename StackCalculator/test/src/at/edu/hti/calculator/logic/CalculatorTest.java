bd599dfea40ab72973083d71711fbdd4cba468df
Gfliess <Gfliess@10.211.55.15>
2013-06-18 00:19:13 +0200

    initial

---
+++ StackCalculator/test/src/at/edu/hti/calculator/logic/CalculatorTest.java
@@ -0,0 +1,15 @@
+package at.edu.hti.calculator.logic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import at.edu.hti.calculator.Calculator;
+import at.edu.hti.calculator.CalculatorException;
+import at.edu.hti.calculator.Calculator.Operation;
+
+public class CalculatorTest {
+
+	
+}
---
+++ StackCalculator/test/src/at/edu/hti/calculator/parser/ParserTest.java
@@ -0,0 +1,16 @@
+package at.edu.hti.calculator.parser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import at.edu.hti.calculator.Calculator;
+import at.edu.hti.calculator.Calculator.Operation;
+
+public class ParserTest {
+
+	
+
+}
